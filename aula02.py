a6f7bd7280905e0c09ee8310fcbbedea95948fcb
Enzo Ferreira <enzo.fm2307@gmail.com>
2025-08-27 12:21:07 -0300

    27/08/2025

---
+++ aula02.py
@@ -0,0 +1,40 @@
+#Operadores
+
+# numero01 = int(input('Digite um numero'))
+# numero02 = int(input('Digite um  segundo numero'))
+
+
+# soma = numero01 + numero02
+# divisão = numero01 / numero02
+# multiplicação = numero01 * numero02
+# subtração = numero01 - numero02
+# sobra = numero01 % numero02
+# divint = numero01 // numero02
+# potenciação = numero01 ** numero02
+# raiz = pow(numero01, 0.5)
+
+# print('A soma é: ', soma) 
+# print('A divisão é: ', divisão) 
+# print('A multiplicação é: ', multiplicação) 
+# print('A subtração é: ', subtração) 
+# print('A sobra é: ', sobra) 
+# print('A divisão inteira é: ', divint)
+# print('A potência é: ', potenciação)
+# print('A raiz quadrada é: ', raiz)
+
+#Funções condicionais
+
+print('-----------------------')
+print('Olá Amigo Friend, Veja o sistema respiratorio')
+print('-----------------------')
+
+hora = int(input('Digite um Horario'))
+
+if hora < 12:
+    print('Bom dia')
+elif hora < 18:
+    print('Bom Tarde')
+else:
+    print("Noite")
+
+
